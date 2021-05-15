2d3b64e9413f806c9a74735c1f507c4093480734
Krister Joas <krister@joas.jp>
2021-05-15 16:05:09 +0900

    Move the 'Commands' class to the 'option' library

---
+++ src/option/Commands.hh
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <functional>
+#include <string>
+#include <map>
+
+#include "parse_args.hh"
+
+namespace wani
+{
+template<typename T>
+class Commands
+{
+public:
+  using function_t = std::function<void(T&, wani::args_range_t)>;
+
+  Commands& command(const std::string& name, function_t cmd)
+  {
+    _commands.emplace(name, cmd);
+    return *this;
+  }
+
+  void parse(T& wanikani, const args_range_t& args)
+  {
+    if(args.first == args.second)
+      usage();
+    auto c = _commands.find(*args.first);
+    if(c == _commands.end())
+      usage();
+    c->second(wanikani, {args.first + 1, args.second});
+  }
+
+private:
+  std::map<std::string, function_t> _commands;
+};
+
+} // namespace wani
--- src/option/parse_args.hh
+++ src/option/parse_args.hh
@@ -5,22 +5,9 @@
 #include <vector>
 #include <map>
 
-#include "usage.hh"
-
 namespace wani
 {
 using args_t = std::vector<std::string>;
 using args_range_t = std::pair<args_t::iterator, args_t::iterator>;
-
-template<typename Base, typename C>
-void parse_command(Base& base, const C& commands, const args_range_t& args)
-{
-  if(args.first == args.second)
-    usage();
-  auto c = commands.find(*args.first);
-  if(c == commands.end())
-    usage();
-  c->second(base, {args.first + 1, args.second});
-}
 
 } // namespace wani
