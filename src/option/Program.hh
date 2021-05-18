c0820a81893eb161f9d7e97873c15a59be6cef69
Krister Joas <krister@joas.jp>
2021-05-18 20:16:06 +0900

    Handle argument lists after options

--- src/option/Program.hh
+++ src/option/Program.hh
@@ -5,9 +5,10 @@
 #pragma once
 
 #include <iostream>
+#include <limits>
 #include <map>
-#include <string>
 #include <memory>
+#include <string>
 #include <tuple>
 
 #include "Option.hh"
@@ -18,29 +19,40 @@ namespace wani::option
 {
 struct Group
 {
+  Group() = default;
+  ~Group() = default;
+  Group(const Group& g) = delete;
+  Group(Group&& g) : min_args(g.min_args), max_args(g.max_args), valid_options(std::move(g.valid_options))
+  {
+    g.min_args = 0;
+    g.max_args = 0;
+  }
+  Group& operator=(Group&& g)
+  {
+    if(this != &g)
+    {
+      min_args = g.min_args;
+      max_args = g.max_args;
+      valid_options = std::move(g.valid_options);
+      g.min_args = 0;
+      g.max_args = 0;
+    }
+    return *this;
+  }
+
+  int min_args = 0;
+  int max_args = 0;
   std::map<std::string, Option> valid_options;
 };
 
-class Arguments
-{
-public:
-  Arguments() = default;
-  Arguments(const args_range_t& range) : range(range) {}
-  args_range_t range;
-};
-
-template<typename... Ts>
 class Program
 {
   std::string _name;
-  using usage_t = std::tuple<const Ts...>;
-  usage_t _usage;
   std::vector<Group> _groups;
   Group _group;
 
 public:
-  Program(Arguments args, const std::string& name): _name(name), _args(args) {}
-  Program(const Ts&&... u): _usage(std::forward<const std::string>(u)...) {}
+  Program(const args_range_t& range, const std::string& name): _name(name), _range(range) {}
 
   template<typename F>
   Program& required(const std::string& name, F f)
@@ -56,143 +68,64 @@ public:
     return *this;
   }
 
-  template<typename F>
-  Program& option(const std::string& name, F f)
-  {
-    _group.valid_options.emplace(name, Option(name, false, f));
-    return *this;
-  }
-
   Program& group()
   {
     _groups.push_back(std::move(_group));
     return *this;
   }
 
-  class Usage
-  {
-  public:
-    template<typename... Args>
-    void operator()(const Args&... args)
-    {
-      if constexpr(sizeof...(args) > 0)
-        wani::option::usage(args...);
-    }
-  };
-
   void usage()
   {
-    if constexpr(std::tuple_size<usage_t>() > 0)
-      std::apply(Usage(), _usage);
-    else
+    std::vector<std::string> help_strings;
+    for(auto& g: _groups)
     {
-      std::vector<std::string> help_strings;
-      for(auto& g: _groups)
+      std::string help{_name};
+      for(auto& [key, o]: g.valid_options)
+        help += " " + o.help();
+      for(auto i = 1; i <= g.min_args; ++i)
+        help += " <arg>";
+      if(g.min_args < g.max_args)
       {
-        std::string help{_name};
-        for(auto& [key, o]: g.valid_options)
-          help += " " + o.help();
-        help_strings.push_back(help);
+        help += " [<arg>";
+        if(g.max_args == std::numeric_limits<int>::max())
+          help += "...]";
+        else
+          help += "]";
       }
-      std::ostringstream os;
-      option::usage(help_strings);
+      help_strings.push_back(help);
     }
+    std::ostringstream os;
+    option::usage(help_strings);
   }
 
-  void args()
+  Program& args()
   {
-    try
-    {
-      auto begin = parse();
-      if(begin == _args.range.second)
-        return;
-    }
-    catch(const std::exception&)
-    {}
-    usage();
+    return group();
   }
 
-  args_range_t args(int min_args)
+  Program& args(int min_args)
   {
-    try
-    {
-      auto begin = parse();
-      if(std::distance(begin, _args.range.second) >= min_args)
-        return {begin, _args.range.second};
-    }
-    catch(const std::exception&)
-    {}
-    usage();
-    throw;
+    _group.min_args = min_args;
+    _group.max_args = std::numeric_limits<int>::max();
+    return group();
   }
 
-  args_range_t args(int min_args, int max_args)
+  Program& args(int min_args, int max_args)
   {
-    try
-    {
-      auto begin = parse();
-      if(auto d = std::distance(begin, _args.range.second); d >= min_args && d <= max_args)
-        return {begin, _args.range.second};
-    }
-    catch(const std::exception&)
-    {}
-    usage();
-    throw;
+    _group.min_args = min_args;
+    _group.max_args = max_args;
+    return group();
   }
 
-  void args(const args_range_t& rest)
+  args_range_t parse()
   {
-    try
-    {
-      auto args = parse(rest);
-      if(args == rest.second)
-        return;
-    }
-    catch(const std::exception&)
-    {}
-    usage();
-  }
-
-  args_range_t args(const args_range_t& rest, int min_args)
-  {
-    try
-    {
-      auto args = parse(rest);
-      if(std::distance(args, rest.second) >= min_args)
-        return {args, rest.second};
-    }
-    catch(const std::exception&)
-    {}
-    usage();
-    throw;
-  }
-
-  args_range_t args(const args_range_t& rest, int min_args, int max_args)
-  {
-    try
-    {
-      auto args = parse(rest);
-      if(auto d = std::distance(args, rest.second); d >= min_args && d <= max_args)
-        return {args, rest.second};
-    }
-    catch(const std::exception&)
-    {}
-    usage();
-    throw;
-  }
-
-private:
-  Arguments _args;
-
-  args_t::iterator parse()
-  {
-    std::vector<std::string> errors;
     _groups.push_back(std::move(_group));
+    std::vector<std::string> errors;
     for(auto& group: _groups)
     {
       try
       {
-        return parse(_args.range, group);
+        return parse(_range, group);
       }
       catch(const argument_error& e)
       {
@@ -204,14 +137,20 @@ private:
     throw;
   }
 
-  args_t::iterator exec(args_t::iterator args, std::vector<Option*>& options)
+private:
+  args_range_t _range;
+
+  args_range_t exec(args_t::iterator args, Group& group, std::vector<Option*>& options)
   {
+    auto distance = std::distance(args, _range.second);
+    if(distance < group.min_args || distance > group.max_args)
+      usage();
     for(const auto* o: options)
       o->exec();
-    return args;
+    return {args, _range.second};
   }
 
-  args_t::iterator parse(const args_range_t& range, Group& group)
+  args_range_t parse(const args_range_t& range, Group& group)
   {
     std::vector<Option*> options;
     Option* current_option = nullptr;
@@ -241,27 +180,16 @@ private:
         }
       }
       else if(*arg == "--")
-        return exec(++arg, options);
+        return exec(++arg, group, options);
       else if(!arg->empty() && arg->at(0) == '-')
         throw argument_error("unknown option: " + *arg);
       else
-        return exec(arg, options);
+        return exec(arg, group, options);
     }
     for(auto& o: group.valid_options)
       if(o.second.required && !o.second.set)
         throw argument_error("missing required argument: " + o.second.name());
-    return exec(arg, options);
-  }
-
-  args_t::iterator parse(const args_range_t& rest)
-  {
-    _groups.push_back(std::move(_group));
-    for(auto& group: _groups)
-    {
-      return parse(rest, group);
-    }
-    usage();
-    throw;
+    return exec(arg, group, options);
   }
 };
 
